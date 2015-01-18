e0c89301ea1cc9bd5d97e2e5eb149bf47cce9e8b
zym <zwdnet@163.com>
2015-01-18 21:28:12 +0800

    完成二叉树，结点删除还有bug，删根节点会出错。

--- Chapter4/binaryTree/BinTree.h
+++ Chapter4/binaryTree/BinTree.h
@@ -28,20 +28,24 @@ class SearchTree
         void printTreePostOrder(TreeNode<T> * Tree);
         int height(TreeNode<T> * Tree);
         TreeNode<T> * insert(T x, TreeNode<T> * Tree);
+        TreeNode<T> * find(T x, TreeNode<T> * Tree);
+        TreeNode<T> * findMin(TreeNode<T> * Tree);
+        TreeNode<T> * findMax(TreeNode<T> * Tree);
+        TreeNode<T> * deleteNode(T x, TreeNode<T> * Tree);
 
     public:
         SearchTree(void){tree = NULL; size = 0;}
         int Size(void){return size;}
         void MakeEmpty(void){makeEmpty(tree);}
-        TreeNode<T> * Find(T x);
-        TreeNode<T> * FindMin(void);
-        TreeNode<T> * FindMax(void);
         void Insert(T x){tree = insert(x, tree);};
-        void Delete(T x);
+        void Delete(T x){deleteNode(x, tree);}
         void PrintTreeInOrder(void){printTreeInOrder(tree);cout<<endl;};
         void PrintTreePreOrder(void){printTreePreOrder(tree);cout<<endl;};
         void PrintTreePostOrder(void){printTreePostOrder(tree);cout<<endl;};
         int Height(void){return height(tree);};
+        bool Find(T x);
+        T FindMin(void){return findMin(tree)->Element;}
+        T FindMax(void){return findMax(tree)->Element;}
 };
 
 template <typename T>
@@ -129,6 +133,110 @@ TreeNode<T> * SearchTree<T>::insert(T x, TreeNode<T> * Tree)
         {
             Tree->Right = insert(x, Tree->Right);
         }
+    }
+    return Tree;
+}
+
+template <typename T>
+bool SearchTree<T>::Find(T x)
+{
+    if (find(x, tree) != NULL)
+    {
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+TreeNode<T> * SearchTree<T>::find(T x, TreeNode<T> * Tree)
+{
+    if (Tree == NULL)
+    {
+        return NULL;
+    }
+    if (x < Tree->Element)
+    {
+        return find(x, Tree->Left);
+    }
+    else if (x > Tree->Element)
+    {
+        return find(x, Tree->Right);
+    }
+    else
+    {
+        return Tree;
+    }
+}
+
+template <typename T>
+TreeNode<T> * SearchTree<T>::findMin(TreeNode<T> * Tree)
+{
+    if (Tree == NULL)
+    {
+        return NULL;
+    }
+    else if (Tree->Left == NULL)
+    {
+        return Tree;
+    }
+    else
+    {
+        return findMin(Tree->Left);
+    }
+}
+
+template <typename T>
+TreeNode<T> * SearchTree<T>::findMax(TreeNode<T> * Tree)
+{
+    if (Tree == NULL)
+    {
+        return NULL;
+    }
+    else if (Tree->Right == NULL)
+    {
+        return Tree;
+    }
+    else
+    {
+        return findMax(Tree->Right);
+    }
+}
+
+template <typename T>
+TreeNode<T> * SearchTree<T>::deleteNode(T x, TreeNode<T> * Tree)
+{
+    TreeNode<T> * TmpCell;
+
+    if (Tree == NULL)
+    {
+        return NULL;
+    }
+    else if (x < Tree->Element)
+    {
+        Tree->Left = deleteNode(x, Tree->Left);
+    }
+    else if (x > Tree->Element)
+    {
+        Tree->Right = deleteNode(x, Tree->Right);
+    }
+    else if (Tree->Left && Tree->Right)  //找到要删除的结点，有两个子结点
+    {
+        TmpCell = findMin(Tree->Right);
+        Tree->Element = TmpCell->Element;
+        Tree->Right = deleteNode(Tree->Element, Tree->Right);
+    }
+    else //只有一个或没有子孩子
+    {
+        TmpCell = Tree;
+        if (Tree->Left == NULL)
+        {
+            Tree = Tree->Right;
+        }
+        else if (Tree->Right == NULL)
+        {
+            Tree = Tree->Left;
+        }
+        delete TmpCell;
     }
     return Tree;
 }
--- Chapter4/binaryTree/main.cpp
+++ Chapter4/binaryTree/main.cpp
@@ -9,9 +9,16 @@ int main()
 {
     SearchTree<int> testTree;
     srand(time(0));
-    for (int i = 0; i < 10; i++)
+    int N;
+    cout<<"输入数据规模:";
+    cin>>N;
+    if (N <= 0)
     {
-        int r = rand()%1000;
+        N = 10;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int r = rand()%(10*N);
         testTree.Insert(r);
     }
     cout<<"中序遍历"<<endl;
@@ -22,5 +29,21 @@ int main()
     testTree.PrintTreePostOrder();
     cout<<"树的高度为:"<<testTree.Height()<<endl;
     cout<<"树的元素总数为:"<<testTree.Size()<<endl;
+    cout<<"输入要查找的数:";
+    int f;
+    cin>>f;
+    if (testTree.Find(f))
+    {
+        cout<<"找到了"<<endl;
+        testTree.Delete(f);
+        cout<<f<<"已删除"<<endl;
+        testTree.PrintTreeInOrder();
+    }
+    else
+    {
+        cout<<"没有找到"<<f<<endl;
+    }
+    cout<<"最大值为:"<<testTree.FindMax()<<endl;
+    cout<<"最小值为:"<<testTree.FindMin()<<endl;
     return 0;
 }
